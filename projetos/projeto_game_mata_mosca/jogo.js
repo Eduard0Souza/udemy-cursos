e871ab55aafc978a6040e9faac7042ac3aaf458a
Eduard0Souza <vesouza1985@gmail.com>
2025-08-08 18:25:53 -0400

    atualização jogo

--- projetos/projeto_game_mata_mosca/jogo.js
+++ projetos/projeto_game_mata_mosca/jogo.js
@@ -1,3 +1,4 @@
+//coletando as dimensões da página
 var altura = 0
 var largura = 0
 function ajustaTamanhoTela(){
@@ -5,5 +6,14 @@ function ajustaTamanhoTela(){
     var largura = window.innerWidth
     console.log(altura, largura)
 }
+ajustaTamanhoTela()
 
+var posicaox = Math.floor(Math.random() * largura)
+var posicaoy = Math.floor(Math.random() * altura)
 
+console.log(posicaox, posicaoy)
+
+//criand elemento html
+var mosca = document.createElement('img')
+mosca.src='imagens/mosca.png '
+document.body.appendChild(mosca)
